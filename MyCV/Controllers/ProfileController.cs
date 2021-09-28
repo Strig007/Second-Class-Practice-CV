84e1b2e1932be46a6ac5f6f3649b414ddf4dc59d
Strig <strigpalma@gmail.com>
2021-09-28 06:42:54 +0600

    Add project files.

---
+++ MyCV/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCV.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Bio()
+        {
+            ViewBag.FirstName = "Strig";
+            ViewBag.LastName = "Palma";
+            ViewBag.DOB = "24-08-1998";
+            ViewBag.Religion = "Christian";
+            ViewBag.Email = "strigpalma@gmail.com";
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
